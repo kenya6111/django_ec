914b11787fb3c022b3e56958793ec647d2ea5bb0
kenya6111 <kenyanke6111@gmail.com>
2024-10-20 15:15:47 +0900

    deproy to heroku

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','']
+ALLOWED_HOSTS = ['127.0.0.1','https://django-ec-277a41e7f0c3.herokuapp.com/']
 
 
 # Application definition
