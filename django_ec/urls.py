d3860f899d4f0a0b3b698acb734cc2f15b204865
kenya6111 <kenyanke6111@gmail.com>
2024-11-02 18:31:30 +0900

    商品管理　編集画面実装

--- django_ec/urls.py
+++ django_ec/urls.py
@@ -3,15 +3,15 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic.base import TemplateView
 from . import views
-from .views import listfunc, detailfunc, adminmenufunc,adminlistfunc,admincreatefunc,admineditfunc,admindeletefunc
+from .views import listfunc, detailfunc, adminmenufunc,adminlistfunc,admincreatefunc,admineditfunc,admindeletefunc,ItemCreate,ItemEdit,ItemDelete
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("list", views.listfunc, name="list"),
     path("detail/<int:pk>", views.detailfunc, name="detail"),
-    path("admin/menu", views.adminmenufunc, name="admin_menu"),
-    path("admin/list", views.adminlistfunc, name="admin_list"),
-    path("admin/create", views.admincreatefunc, name="admin_create"),
-    path("admin/edit", views.admineditfunc, name="admin_edit"),
-    path("admin/delete", views.admindeletefunc, name="admin_delete"),
+    path("admin/menu", views.adminmenufunc, name="menu"),
+    path("admin/list", views.adminlistfunc, name="list"),
+    path('admin/create', ItemCreate.as_view(), name="create"),
+    path("admin/edit/<int:pk>", ItemEdit.as_view(), name="edit"),
+    path("admin/delete/<int:pk>", ItemDelete.as_view(), name="delete"),
 ]
--- django_ec/views.py
+++ django_ec/views.py
@@ -4,6 +4,8 @@ from config.settings import BASE_DIR
 from django_ec.models import ItemModel
 import environ
 from .constants.consts import Menu
+from django.views.generic import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -34,11 +36,80 @@ def adminmenufunc(request):
     return render(request, 'django_ec/admin/menu.html', {'menu_list':menu_list})
 
 def adminlistfunc(request):
-    object_list = ItemModel.objects.all()
-    return render(request, 'django_ec/admin/list.html', {'object_list':object_list})
+    if request.method == 'POST':
+        product_name = request.POST.get("product-name")
+        star_from = request.POST.get("star_from")
+        star_to = request.POST.get("star_to")
+        price = request.POST.get("price")
+        is_sale = request.POST.get("is-sale")
+        create_date_from = request.POST.get("create-date-from")
+        create_date_to = request.POST.get("create-date-to")
+
+        object_list = ItemModel.objects.all()
+
+        error_list=[]
+        validate(error_list, star_from, star_to)
+
+        if product_name is not None:
+            object_list = object_list.filter(name__contains=product_name)
+        
+        # if star is not None:
+        #     star = object_list.filter(name=star)
+        # if price is not None:
+        #     object_list = object_list.filter(name=price)
+        # if product_name is not None:
+        #     object_list = object_list.filter(name=product_name)
+
+
+        return render(request, 'django_ec/admin/list.html', {'object_list':object_list, 'error_list':error_list})
+        # user = authenticate(request, username=username, password=password)
+        # if user is not None:
+        #     login(request, user)
+        #     return render(request, 'login.html', {'context':'logged in !!'})
+        # else:
+        #     return render(request, 'login.html', {'context':' not logged in !!'})
+
+    # return render(request, 'login.html', {'context':'get method '})
+    return render(request, 'django_ec/admin/list.html', {})
 def admincreatefunc(request):
     return render(request, 'django_ec/admin/create.html', {})
-def admineditfunc(request):
-    return render(request, 'django_ec/admin/edit.html', {})
+def admineditfunc(request,pk):
+    if request.method == 'POST':
+
+        return render(request, 'django_ec/admin/list.html', {'object':object})
+
+    else:
+        # GETの場合
+        object = get_object_or_404(ItemModel, pk=pk)
+        return render(request, 'django_ec/admin/edit.html', {'object':object})
+
+    return render(request, 'django_ec/admin/edit.html', {'object':object})
 def admindeletefunc(request):
     return render(request, 'django_ec/admin/delete.html', {})
+
+class ItemCreate(CreateView):
+    template_name = 'django_ec/admin/create.html'
+    model = ItemModel
+    fields = ('name','star','image','price','is_sale')
+    success_url = reverse_lazy('list') # データの作成完了した後の遷移先
+
+    def form_invalid(self, form):
+        print(form.errors)  # デバッグ用にエラー内容を出力
+        return super().form_invalid(form)
+
+class ItemEdit(UpdateView):
+    template_name = 'django_ec/admin/edit.html'
+    model = ItemModel
+    fields = ('name','star','image','price','is_sale')
+    success_url = reverse_lazy('list')
+
+class ItemDelete (DeleteView):
+    template_name = 'django_ec/admin/delete.html'
+    model = ItemModel
+    success_url = reverse_lazy('list')
+
+
+def validate(error_list, star_from, star_to):
+    if star_from is not None and star_to is not None:
+        if star_from >= star_to:
+            return error_list.append("星数の開始数は終了数より小さい値を設定してください")
