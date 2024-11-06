e0cc9fccdf3c5e42bbc9da20374172592251ee2e
kenya6111 <kenyanke6111@gmail.com>
2024-11-06 21:58:00 +0900

    basicauthのmiddleware削除

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'basicauth.middleware.BasicAuthMiddleware',
+    # 'basicauth.middleware.BasicAuthMiddleware',
 ]
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 
